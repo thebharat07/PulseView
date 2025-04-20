0a6a7f53968cd08c70d77209cb511f6612d7cf81
Bongu Hari Hara Charan <100521280+Chintu1308@users.noreply.github.com>
2025-04-20 09:08:21 +0530

    Update content.js

--- content.js
+++ content.js
@@ -21,7 +21,7 @@ function initWidget() {
   widget.style.boxSizing = "border-box";
 
   widget.innerHTML = `
-    <div id="widget-header" style="cursor: move; font-weight: bold;">PulseView 📊</div>
+    <div id="widget-header" style="cursor: move; font-weight: bold;">PulseView</div>
     <canvas id="reqChart" style="margin-top: 10px; width:100%; height:180px;"></canvas>
     <div id="log" style="margin-top: 10px; max-height: 100px; overflow-y: auto;"></div>
   `;
